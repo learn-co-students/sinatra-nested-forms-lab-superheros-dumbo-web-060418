98de2c6cb7baa62f45e15d78c77d549dc9cf3004
mark sauer-utley <mark.utley.713@gmail.com>
2018-06-26 17:39:14 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,17 +9,10 @@ class App < Sinatra::Base
     end
 
     post "/teams" do
-      @team_name = params[:team][:name]
-      @team_motto = params[:team][:motto]
-      @hero1_name = params[:team][:members][0][:name]
-      @hero1_power = params[:team][:members][0][:power]
-      @hero1_bio = params[:team][:members][0][:biography]
-      @hero2_name = params[:team][:members][1][:name]
-      @hero2_power = params[:team][:members][1][:power]
-      @hero2_bio = params[:team][:members][1][:biography]
-      @hero3_name = params[:team][:members][2][:name]
-      @hero3_power = params[:team][:members][2][:power]
-      @hero3_bio = params[:team][:members][2][:biography]
+      @team = params[:team]
+      @hero1 = params[:team][:members][0]
+      @hero2 = params[:team][:members][1]
+      @hero3 = params[:team][:members][2]
 
       erb :team
     end
